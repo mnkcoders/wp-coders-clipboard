fbd545e25a201c8c8fdd36381bf85be803cf4fd8
Coder#Laptop <mnkcoders@gmail.com>
2024-06-10 14:32:35 +0200

    Admin uploader view setup

--- admin/main.php
+++ admin/main.php
@@ -3,11 +3,17 @@
 <div class="wrap coders-clipboard main">
     <h1 class="wp-heading-inline"><?php print get_admin_page_title() ?></h1>
     <p><?php print self::class; ?></p>
-    <div class="clipboard">
+    <div class="clipboard container">
+        <div class="uploader container">
+            <form name="uploader" action="<?php print $this->url_upload ?>" method="post" enctype="multipart/form-data">
+                <label for="id_files" class="button files">Select files</label>
+                <input id="id_files" type="file" name="upload[]" class="hidden" />
+                <button type="submit" name="upload" id="upload-button">Upload</button> 
+            </form>
+        </div>
         <ul class="image-clipboard inline hidden" id="clipboard-capture">
             <!-- add images from clipboard here -->
         </ul>
-        <button type="button" name="upload" id="upload-button">Upload</button>
     </div>
     <div class="uploader">
         
