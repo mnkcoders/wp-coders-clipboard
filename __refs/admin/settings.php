dc048ce5d27f9f4a0a6059db4b9d29055d541b42
Coder01 <mnkcoders@gmail.com>
2025-05-07 08:10:19 +0200

    Removed old refs

--- __refs/admin/settings.php
+++
@@ -1,7 +0,0 @@
-<?php defined('ABSPATH') or die ?>
-<!-- CODERS CLIPBOARD CONTAINER OPENER -->
-<div class="wrap coders-clipboard settings">
-    <h1 class="wp-heading-inline"><?php print get_admin_page_title() ?></h1>
-    <p><?php print self::class; ?></p>
-</div>
-<!-- CODERS CLIPBOARD CONTAINER CLOSER -->
--- __refs/public/main.php
+++
@@ -1,6 +0,0 @@
-<?php defined('ABSPATH') or die ?>
-<!-- CODERS CLIPBOARD CONTAINER OPENER -->
-<div class="coders-clipboard">
-    
-</div>
-<!-- CODERS CLIPBOARD CONTAINER CLOSER -->
